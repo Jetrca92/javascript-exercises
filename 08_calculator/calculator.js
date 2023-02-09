9f9bba3070fe2ef2cb33730d6c86502d7bbf1560
Jetrca92 <blaz.guzej@gmail.com>
2023-02-09 17:33:59 +0100

    Complete ex8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(x, y) {
+  return x + y;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+	for (let i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for (let i = 0; i < array.length; i++) {
+    result *= array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x === 0) {
+    return 1;
+  }
+	let result = x;
+  for (let i = 1; i < x; i++) {
+    result *= x - i;
+  }
+  return result;
 };
 
 // Do not edit below this line
