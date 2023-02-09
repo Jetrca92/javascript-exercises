5b0c29791a06813e99b28ebd363bd1a5e6720ecd
Jetrca92 <blaz.guzej@gmail.com>
2023-02-09 16:54:06 +0100

    Complete exercise 4 and 5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var returnString = "";
+    if (Math.sign(num) === -1) {
+        return "ERROR"
+    }
+    for (i = 0; i < num; i++) {
+        returnString += string;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0) {
+        return "ERROR";
+    }
+    if (typeof(x) != "number" || typeof(y) != "number") {
+        return "ERROR";
+    }
+    if (y < x) {
+        let len = x - y;
+        let total = 0;
+        for (let i = 0; i <= len; i++) {
+            total += y + i;
+        }
+        return total;
+    }
+    let len = y - x;
+    let total = 0;
+    for (let i = 0; i <= len; i++) {
+        total += x + i;
+    }
+    return total;
 };
 
 // Do not edit below this line
