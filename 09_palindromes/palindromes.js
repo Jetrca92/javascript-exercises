540c32188edcad9d5217ed89eb03a199baec635c
Jetrca92 <blaz.guzej@gmail.com>
2023-02-10 17:05:09 +0100

    Complete ex9 ex10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let lettersString = string.replace(/[^a-zA-Z]/gm,"").toLowerCase();
+    let revString = "";
+    for (let i = lettersString.length - 1; i >= 0; i--) {
+        revString += lettersString[i];
+    }
+    if (revString === lettersString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
