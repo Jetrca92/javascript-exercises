8b2f44d44176829544ef89d2eb7fbd74567b6bb8
Jetrca92 <blaz.guzej@gmail.com>
2023-02-12 15:54:48 +0100

    Complete all exercises

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,31 @@
-const caesar = function() {
-
+const caesar = function(text, num) {
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+        const charCode = text.charCodeAt(i);
+        if (num < 0) {
+            num = num % 26;
+            if (charCode >= 65 && charCode <= 90) {
+                result += String.fromCharCode(((charCode - 65 - (Math.abs(num) % 26) + 26) % 26) + 65);
+                } 
+                else if (charCode >= 97 && charCode <= 122) {
+                    result += String.fromCharCode(((charCode - 97 - (Math.abs(num) % 26) + 26) % 26) + 97);
+                } 
+                else {
+                result += text.charAt(i);
+              }
+        }
+        else {
+            if (charCode >= 65 && charCode <= 90) {
+                result += String.fromCharCode(((charCode - 65 + num) % 26) + 65);
+              } else if (charCode >= 97 && charCode <= 122) {
+                result += String.fromCharCode(((charCode - 97 + num) % 26) + 97);
+              } else {
+                result += text.charAt(i);
+              }
+            }
+        }
+        
+      return result;
 };
 
 // Do not edit below this line
