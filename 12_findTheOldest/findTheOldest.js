24dfc690c89f3596f14aa7d18dd5bbf6557ac7fa
Jetrca92 <blaz.guzej@gmail.com>
2023-02-12 15:32:35 +0100

    Complete ex11, ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestName = "";
+    let oldest = 0;
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath) {
+            if ((people[i].yearOfDeath - people[i].yearOfBirth) > oldest) {
+                oldest = people[i].yearOfDeath - people[i].yearOfBirth;
+                oldestName = people[i].name;
+            }
+        }
+        else {
+            if ((2023 - people[i].yearOfBirth) > oldest) {
+                oldest = 2023 - people[i].yearOfBirth;
+                oldestName = people[i].name;
+            }
+        }
+    }
+    return oldestName;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
